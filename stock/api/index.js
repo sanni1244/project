ec098d50fe79a4b7e6e26655aa1ed6db56530364
Opeyemi <astro8va@gmail.com>
2025-04-29 22:58:37 +0100

    mongo set up

--- stock/api/index.js
+++ stock/api/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 require('dotenv').config();
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,12 +13,10 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-// Example route
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
